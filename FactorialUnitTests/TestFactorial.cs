bc376562360c3ff9c173ca1d505e687eec7d8bc6
Marko Boroš <maboros@vsite.hr>
2021-11-08 18:53:36 +0100

    Fix factorial

--- FactorialUnitTests/TestFactorial.cs
+++ FactorialUnitTests/TestFactorial.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
-namespace Factorial.FactorialUnitTests
+namespace Factorial.UnitTests
 {
     [TestClass]
     public class TestFactorial
